6e4ee7e3dea01da075349ac874060a983502df8b
Dabble <ddabble@users.noreply.github.com>
2022-10-02 20:17:32 +0200

    Refactored cron job code to function

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ set -euo pipefail
 home_dir=$(readlink -f "..")
 # Get the directory containing this file, resolving symlinks (based on code from https://stackoverflow.com/a/51651602)
 repo_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-update_script="$repo_dir/cron-jobs/weekly/update-software"
 octoprint_python_executable="$home_dir/oprint/bin/python"
 
 if [[ "$(whoami)" = "root" ]]; then
@@ -18,6 +17,18 @@ elif [ ! -e "$octoprint_python_executable" ]; then
   >&2 echo "is this project not placed/configured correctly, or has OctoPrint/OctoPi changed setup?"
   exit 1
 fi
+
+register_and_run_weekly_cron_job() {
+  local cron_job_script="$1"
+
+  local script_filename
+  script_filename=$(basename "$cron_job_script")
+  local link_name="/etc/cron.weekly/$script_filename"
+  # Register the cron job
+  ln -sf "$cron_job_script" "$link_name"
+  # Execute the cron job script
+  eval "$link_name"
+}
 
 # This is mainly done to not have to re-input the password for `sudo` after having upgraded packages,
 # as that often takes long enough to make the OS re-prompt for the password
@@ -31,10 +42,8 @@ raspi-config nonint do_camera 0
 
 python3 -m python_scripts.customize_octopi_config
 
-# Register the cron job
-ln -sf "$update_script" /etc/cron.weekly/update-software
-# Execute the cron job script immediately
-eval "$update_script"
+$(declare -f register_and_run_weekly_cron_job)
+register_and_run_weekly_cron_job "$repo_dir/cron-jobs/weekly/update-software"
 
 
 reboot
